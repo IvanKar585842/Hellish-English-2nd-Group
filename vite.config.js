e4fe37ebb45a5a5a87dfaa91692acf6413ab1151
IvanKar585842 <karpenkoivanb@gmail.com>
2025-09-11 23:13:34 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import glob from 'glob'
-import injectHTML from 'vite-plugin-html-inject'
-import FullReload from 'vite-plugin-full-reload'
+// import injectHTML from 'vite-plugin-html-inject'
+// import FullReload from 'vite-plugin-full-reload'
 
 export default defineConfig({
   define: {
@@ -14,5 +14,5 @@ export default defineConfig({
     },
     outDir: '../dist',
   },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  // plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 })
